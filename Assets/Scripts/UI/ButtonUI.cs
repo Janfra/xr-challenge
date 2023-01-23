ea8310bd4fc685ee159c7e90ffe046cb222ad8eb
Janfra <janfrancoherrera@gmail.com>
2023-01-23 16:04:58 +0000

    Button UI WIP
    Worked on the basics of the button UI and organised the UI scripts folder
    Changes / Updates:
    - On selected event added to handle selecting logic
    - On deselected event added to handle deselecting logic
    - On pointer enter event added to select button on hover
    - Changes text colour based on selected or unselected
    - Enables and disables a pointer to further highlight currently selected button

--- Assets/Scripts/UI/ButtonUI.cs
+++ Assets/Scripts/UI/ButtonUI.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ButtonUI : MonoBehaviour, ISelectHandler, IDeselectHandler
+public class ButtonUI : MonoBehaviour, ISelectHandler, IDeselectHandler, IPointerEnterHandler
 {
+    #region Dependencies
+
     [Header("Dependencies")]
     [SerializeField]
     private Button button;
     [SerializeField]
     private Text text;
+    [SerializeField]
+    private RectTransform selectionPointer;
+
+    #endregion
+
+    #region Config
+
+    [Header("Config")]
+    [SerializeField]
+    private Color deselectTextColour;
+    [SerializeField]
+    private Color selectTextColour;
+
+    #endregion
 
     private const float DESELECT_OPACITY = 0.3f;
 
@@ -17,19 +33,102 @@ public class ButtonUI : MonoBehaviour, ISelectHandler, IDeselectHandler
         if(button == null)
         {
             button = GetComponent<Button>();
+            Debug.LogError($"No button set in {gameObject.name}!");
+        }
+        if(text == null)
+        {
+            text = GetComponentInChildren<Text>();
+            Debug.LogError($"No text set in {gameObject.name}!");
+        }
+        if(selectionPointer == null)
+        {
+            selectionPointer = GetComponentInChildren<RectTransform>();
+            Debug.LogError($"No selection pointer set in {gameObject.name}!");
+        }
+
+        SetDeselectTextColour();
+        SelectTextColourClamp();
+        SetPointerMiddleLeftAnchor();
+    }
+
+    /// <summary>
+    /// Select this button on cursor enter
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        button.Select();
+    }
+
+    /// <summary>
+    /// Change text colour and points at button on select
+    /// </summary>
+    /// <param name="eventData"></param>
+    void ISelectHandler.OnSelect(BaseEventData eventData)
+    {
+        text.color = selectTextColour;
+        SetPointerPosition(true);
+    }
+
+    /// <summary>
+    /// Change text colour and disable pointer on deselect
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnDeselect(BaseEventData eventData)
+    {
+        SetDeselectTextColour();
+        SetPointerPosition(false);
+    }
+
+    /// <summary>
+    /// Sets the text colour to the deselected colour.
+    /// </summary>
+    private void SetDeselectTextColour()
+    {
+        Color newColour = deselectTextColour;
+        newColour.a = DESELECT_OPACITY;
+        text.color = newColour;
+    }
+
+    /// <summary>
+    /// Clamps the selected text colour to be fully visible at start.
+    /// </summary>
+    private void SelectTextColourClamp()
+    {
+        Color opacityClamp = selectTextColour;
+        opacityClamp.a = 1;
+        selectTextColour = opacityClamp;
+    }
+
+    /// <summary>
+    /// Sets the pointer position to be on the left side of the button.
+    /// </summary>
+    /// <param name="_isActive"></param>
+    private void SetPointerPosition(bool _isActive)
+    {
+        selectionPointer.gameObject.SetActive(_isActive);
+
+        if (_isActive)
+        {
+            float xPosOffset = -(selectionPointer.rect.width + selectionPointer.rect.width / 3);
+            Vector3 positionNextToButton = new Vector3(xPosOffset, 0);
+            selectionPointer.anchoredPosition = positionNextToButton;
+        }
+        else
+        {
+            selectionPointer.anchoredPosition = Vector3.zero;
         }
     }
 
-    void ISelectHandler.OnSelect(BaseEventData eventData)
+    /// <summary>
+    /// Sets the pointer anchor settings to be to the middle left.
+    /// </summary>
+    private void SetPointerMiddleLeftAnchor()
     {
-        text.color = Color.black;
+        selectionPointer.anchorMin = new Vector2(0f, 0.5f);
+        selectionPointer.anchorMax = new Vector2(0f, 0.5f);
+        selectionPointer.pivot = new Vector2(0f, 0.5f);
+        selectionPointer.anchoredPosition = Vector3.zero;
+        selectionPointer.gameObject.SetActive(false);   
     }
-
-    public void OnDeselect(BaseEventData eventData)
-    {
-        Color newColour = Color.white;
-        newColour.a = DESELECT_OPACITY; 
-        text.color = newColour;
-    }
-    
 }
